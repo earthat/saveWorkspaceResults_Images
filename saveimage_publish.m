78678d2f494267d712537f25381197190e3ce89c
Abhishek <earthat2005@gmail.com>
2021-08-09 16:22:52 +0530

    Update saveimage_publish.m

--- saveimage_publish.m
+++ saveimage_publish.m
@@ -2,4 +2,4 @@ function saveimage_publish(imagename)
 fig = gcf;
 fig.PaperUnits = 'inches';
 % fig.PaperPosition = [0 0 7 3];
-print(strcat(imagename),'-dpng','-r0')
+print(strcat(imagename),'-dpng','-r600')
